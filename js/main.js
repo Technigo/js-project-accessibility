f866f2a9ef2430e8e91dfcd4588245325091c8c7
llindallarsson <larsson.ll@hotmail.com>
2025-03-31 14:03:42 +0200

    main menu styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// function displayActiveMenu() {
+//   const menuItems = document.querySelectorAll("ul li");
+//   const currentPath = window.location.pathname;
+
+//   menuItems.forEach((item) => {
+//     const link = item.querySelector("a");
+//     const linkPath = link.getAttribute("href");
+
+//     // Check if the link's href matches the current URL path
+//     if (currentPath.endsWith(linkPath)) {
+//       item.classList.add("active");
+//     } else {
+//       item.classList.remove("active");
+//     }
+//     // Add click event listener (for in-page navigation)
+//     item.addEventListener("click", () => {
+//       // Remove "active" from any other item
+//       document.querySelectorAll("li.active").forEach((activeItem) => {
+//         activeItem.classList.remove("active");
+//       });
+//       // Add "active" to the clicked item
+//       item.classList.add("active");
+//     });
+//   });
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
   const startQuizButton = document.getElementById("start-quiz-button");
   const quizSection = document.querySelector(".quiz-section");
@@ -16,4 +42,5 @@ document.addEventListener("DOMContentLoaded", () => {
       "Could not find one or more of the required elements: startQuizButton, quizSection, introSection"
     );
   }
+  displayActiveMenu();
 });
