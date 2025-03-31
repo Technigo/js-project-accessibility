9e37b5828cc7f870b4cf4a84edb4bd934cb59aa8
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-03-31 15:51:56 +0200

    Var tvungen att fixa en funktion för att ge form en klass för att visa en markering som vid interaktiva element, men endast via skip-länken

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 //DOM ELEMENT
 const form = document.getElementById('quiz-form');
+
+// Listen to skip a link click
+document.querySelector('.skip-link').addEventListener('click', () => {
+  form.classList.add('show-focus');
+
+  target.scrollIntoView({ behavior: 'smooth', block: 'start' }); //Osäker på denna det var något för mobil
+});
