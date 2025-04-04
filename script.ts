a4e125a748bd04398f8c9975e9fa9cf2ac240a25
kidfromcalifornia <hello.jonnyhicks@gmail.com>
2025-04-04 10:11:18 +0200

    reverted changes

--- script.ts
+++ script.ts
@@ -61,7 +61,7 @@ function loadQuestion(): void {
     btn.name = "option";
     btn.id = `option-${i}`; // Unique ID for accessibility
     btn.value = `${i + 1}. ${element}`;
-    btn.tabIndex = 0;
+    btn.tabIndex = -1;
     btn.setAttribute("aria-labelledby", `label-${i}`); //V Uses aria-labelledby for better screen reader support
     btn.setAttribute("role", "radio"); //V     
 
@@ -99,8 +99,8 @@ function loadQuestion(): void {
     const label = document.createElement("label");
     label.htmlFor = btn.id;
     label.id = `label-${i}`;
-    //label.tabIndex = 0;
-    label.onkeyup = enterSubmit(event);
+    label.tabIndex = 0;
+
 
     label.appendChild(btn);
     label.append(` ${element}`)
