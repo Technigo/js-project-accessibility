756b4ea11b1e93a9bfe596fb6f00ef92ed82459e
plazadevina <plazadevina@gmail.com>
2025-08-08 08:32:04 +0000

    git commit -m "Code-lab-web"

--- js/sever.js
+++ js/sever.js
@@ -1,21 +1,9 @@
-// Import required dependencies
-const express = require('express');
-const path = require('path');
-
-// Initialize Express application
-const app = express();
-const port = 3000;
-
-// Configure static file serving
-// This middleware serves all files from the root directory
-// Examples: HTML, CSS, JavaScript, images, etc.
-app.use(express.static(path.join(__dirname)));
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-  console.log(`Serving static files from: ${__dirname}`);
-});
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import crypto from 'crypto'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl =process.env_MONGO_URL II "mongodb://localhost/auth"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useundefinedtopology: true})
