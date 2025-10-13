29722b17ed4de92f46eb243ceed0979fc73f9902
plazadevina <plazadevina@gmail.com>
2025-10-13 15:51:56 +0000

    git commit -m "Code-lab-web"

--- .idx/views/routes/auth.js
+++ .idx/views/routes/auth.js
@@ -3,8 +3,6 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook');
 var session = require('express-session');
 var passport = require('passport');
-var db = require('../db');
-var router = express.Router();
 passport.use(new FacebookStrategy({
   clientID: process.env['FACEBOOK_CLIENT_ID'],
   clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
@@ -13,6 +11,70 @@ passport.use(new FacebookStrategy({
 }, function verify(accessToken, refreshToken, profile, cb) {
   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
     'https://www.facebook.com',
+    profile.id
+  ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) {
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+        
+        var id = this.lastID;
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          'https://www.facebook.com',
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          var user = {
+            id: id,
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
+      });
+    }
+  });
+}));
+var db = require('../db');
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+var router = express.Router();
+passport.use(new FacebookStrategy({
+  clientID: process.env['FACEBOOK_CLIENT_ID'],
+  clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/facebook',
+  router.get('/login/federated/facebook', passport.authenticate('facebook'));
+  state: true
+}, function verify(accessToken, refreshToken, profile, cb) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [n
+    router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    }));
+    'https://www.facebook.com',
+    router.post('/logout', function(req, res, next) {
+      req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+    });
     profile.id
   ], function(err, row) {
     if (err) { return cb(err); }
