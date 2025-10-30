cda20b4a2bcf0668025bb1e88b24bc9fc91b70ca
Arta Kjato <artakjato@gmail.com>
2025-10-30 23:04:42 +0100

    will add submission form reset

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const adoptionForm = document.getElementById('adoption-form');
 const responseSection = document.getElementById('response');
 const responseContent = document.getElementById('response-content');
+const submissionForm = document.getElementById('submission-form');
 let userName = '';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const submissionForm = document.getElementById('submission-form');
 })
 
 adoptionForm.addEventListener('submit', (e) => {
@@ -15,12 +15,14 @@ adoptionForm.addEventListener('submit', (e) => {
   responseSection.scrollIntoView({ behavior: 'smooth' });
   let response =
     `Thank you for your interest <b>${userName}</b>. A member of our staff will contact you shortly.`;
-  
+
   responseSection.hidden = false;
   responseContent.innerHTML = response;
 
   responseSection.setAttribute('tabindex', '-1');
   responseSection.focus();
+
+  submissionForm.reset();
 });
 
 
