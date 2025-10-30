939a9b70cc326496591157efbb960a2bcbeba3c6
Arta Kjato <artakjato@gmail.com>
2025-10-30 22:56:26 +0100

    will add changes to html, css and js regarding form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+const adoptionForm = document.getElementById('adoption-form');
+const responseSection = document.getElementById('response');
+const responseContent = document.getElementById('response-content');
+let userName = '';
+
+document.addEventListener('DOMContentLoaded', () => {
   const submissionForm = document.getElementById('submission-form');
-    let userName = '';
 })
+
+adoptionForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  userName = document.getElementById('name').value;
+  responseSection.hidden = false;
+  responseSection.scrollIntoView({ behavior: 'smooth' });
+  let response =
+    `Thank you for your interest <b>${userName}</b>. A member of our staff will contact you shortly.`;
+  
+  responseSection.hidden = false;
+  responseContent.innerHTML = response;
+
+  responseSection.setAttribute('tabindex', '-1');
+  responseSection.focus();
+});
+
+
 
